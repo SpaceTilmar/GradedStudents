c974005322189750b82dcd1213f765eac8c83668
SpaceTilmar <21tilmaro@gmail.com>
2023-09-17 16:40:30 -0400

    Updated files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,18 @@ package org.example;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Double[] examScores = {190.0, 120.0, 90.0, 150.0, 150.0};
+        ArrayList<Double> examScores = new ArrayList<>();
+        examScores.add(100.00);
+        examScores.add(95.00);
+        examScores.add(123.00);
+        examScores.add(96.00);
         Student student = new Student("Tilmar", "Overbeck", examScores);
         student.getNumberOfExamsTaken();
+        student.addExamScore(60.00);
+        student.setExamScores(1, 59.00);
+
+        System.out.println(student.toString());
+
 
     }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -6,9 +6,9 @@ public class Student {
     private String firstName;
     private String lastName;
 
-    private ArrayList<Double> examScores = new ArrayList<>();
+    private ArrayList<Double> examScores;
 
-    public Student(String firstName, String lastName, Double[] examScores) {
+    public Student(String firstName, String lastName, ArrayList<Double> examScores) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.examScores = examScores;
@@ -29,15 +29,37 @@ public class Student {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    public int void getNumberOfExamsTaken() {
-        return int this.examScores.size();
+    public int getNumberOfExamsTaken() {
+        return examScores.size();
     }
     public String getExamScores(){
-        String output =
-        for(Double score : examScores){
-            System.out.println("Exam " + score);
+        String exam = "Exam Scores: \n";
+
+        for(int i = 0; i < examScores.size(); i++){
+            exam += "Exams " + (i+1) + "-> " + examScores.get(i) + "\n";
         }
+        return exam;
+
     }
 
+    public void addExamScore(double examScore){
+        examScores.add(examScore);
+    }
+    public void setExamScores(int examNumber, double newScore){
+        examScores.set(examNumber, newScore);
+    }
+    public Double getAverageExamScore(){
+        double sum = 0.0;
+        for(int x = 0; x < examScores.size(); x++){
+            sum += examScores.get(x);
+        }
+        return sum / examScores.size();
+    }
 
+    @Override
+    public String toString() {
+        return "Student Name: " + firstName + ' ' +  lastName + "\n" +
+                "> Average Score: " + getAverageExamScore() + "\n"
+                + "> Exam Scores: " + "\n" + getExamScores();
+    }
 }
